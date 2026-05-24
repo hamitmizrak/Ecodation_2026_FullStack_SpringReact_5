8537f52f8d5e7de5fdae3b5c6b935aa629788575
Hamit Mızrak <hamitmizrak@gmail.com>
2026-05-24 10:33:33 +0300

    CAST,OPERATORS

--- assets/js/template.js
+++ assets/js/template.js
@@ -58,6 +58,7 @@ React               NodeJS
 // var globalVariable = 'Merhabalar';
 // console.log(globalVariable);
 
+//**** GLOBAL/LOCAL ***********************
 // var globalVariable = 44;
 // console.log(globalVariable);
 
@@ -67,6 +68,7 @@ React               NodeJS
 // globalVariable = true;
 // console.log(globalVariable);
 
+// ES6 (2015)
 // Yapılmaz
 // let localVariable = 44
 // console.log(localVariable);
@@ -74,15 +76,67 @@ React               NodeJS
 // let localVariable = 25;
 // console.log(localVariable);
 
-let localVariable = 44;
-console.log(localVariable);
+// let localVariable = 44;
+// console.log(localVariable);
 
-localVariable = 23;
-console.log(localVariable);
+// localVariable = 23;
+// console.log(localVariable);
 
-const localVariable2 = 1453;
-console.log(localVariable2);
-localVariable2 = 99;
-console.log(localVariable2);
+// const localVariable2 = 1453;
+// console.log(localVariable2);
+// localVariable2 = 99;
+// console.log(localVariable2);
 
+//**** TYPE ***********************
+// let data1="merhabalar veriler"
+// console.log(data1)
+// console.log(typeof data1)
 
+// let data2 = 44;
+// console.log(data2);
+// console.log(typeof data2);
+
+// let data3 = 44.23;
+// console.log(data3);
+// console.log(typeof data3);
+
+// let data4 = 3>1;
+// console.log(data4);
+// console.log(typeof data4);
+
+//**** VARIABLE ROLES ***********************
+// üğşçöİÜĞŞÇÖ
+// let $_data5="merhabalar veriler"
+// console.log($_data5);
+
+//**** COMMON OBJECT ************************
+let vocabulary = 'Javascript öğreniyorum';
+let condition = true; // 3<1
+let number = 44;
+let floatingPoint = 44.23;
+let object = {};
+let arrayData = [4, 5, 6, 8, 'merhabalar', true];
+
+//**** OPERATORS ************************
+let number1 = 23,
+  number2 = 3;
+
+console.log('Toplama: ' + (number1 + number2));
+console.log('Çıkarma: ' + (number1 - number2));
+console.log('Çarpma: ' + (number1 * number2));
+console.log('Bölme: ' + (number1 / number2));
+console.log('Kalan:' + (number1 % number2));
+
+// =
+// ==
+// ===
+// & = VE
+// | = VEYA
+
+//**** CAST ************************
+// Number() ==> sayıya çevirir
+// String() ==> kelimeye çevirir
+let number6 = '20',
+  number7 = 40;
+console.log(number6 + number7);
+console.log(Number(number6) + number7);
