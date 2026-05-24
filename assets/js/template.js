16df9942761ac21083406c9c701b929c8b577cc4
Hamit Mızrak <hamitmizrak@gmail.com>
2026-05-24 09:56:16 +0300

    javascript

---
+++ assets/js/template.js
@@ -0,0 +1,88 @@
+// Single commet
+
+/* multiple comment */
+
+/*
+1995 Brendan Eich tarafından geliştiriliyor
+Single thread
+Betik diller ailesinden interpreter(yorumlama)
+web side çalışan ancak Nodejs yapısıyla Server Side
+*/
+
+/*
+Dynamics types
+OOP
+Cross Platform
+Lightweight
+*/
+
+/*
+Kullanan şirket
+- Google
+- eBay
+- Facebook
+- Linkedin
+- Netflix
+- PayPal
+- E-Devlet
+- Tesla
+*/
+
+/* JavaScript
+
+Libraries            Framework
+---------           -----------
+jQuery              Angular
+React               NodeJS
+                    Vue js
+*/
+
+//**** ÇIKTI ****************************
+// console.log('Console log');
+// console.info('Console info');
+// console.warn('Console warning');
+// console.error('Console error');
+
+// window.alert("Window alert")
+// alert("Deneme")
+
+//**** USER ****************************
+// var userVariable = prompt("Adınız Soyadınız");
+// document.write("Adınız & Soyadınız: " + userVariable+ "<br/>Diğer satır");
+
+// "" double quotes
+// '' single quotes
+// `` backtick
+
+//**** USER ****************************
+// var globalVariable = 'Merhabalar';
+// console.log(globalVariable);
+
+// var globalVariable = 44;
+// console.log(globalVariable);
+
+// globalVariable = 44.23;
+// console.log(globalVariable);
+
+// globalVariable = true;
+// console.log(globalVariable);
+
+// Yapılmaz
+// let localVariable = 44
+// console.log(localVariable);
+
+// let localVariable = 25;
+// console.log(localVariable);
+
+let localVariable = 44;
+console.log(localVariable);
+
+localVariable = 23;
+console.log(localVariable);
+
+const localVariable2 = 1453;
+console.log(localVariable2);
+localVariable2 = 99;
+console.log(localVariable2);
+
+
